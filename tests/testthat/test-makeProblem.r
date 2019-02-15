1d6e45c40b3d53117ae93dcd77a8b46d75314c3f
Bernhard Meindl <bernhard.meindl@gmail.com>
2019-02-15 23:06:46 +0100

    new testthat syntax; syntax linting

--- tests/testthat/test-makeProblem.r
+++ tests/testthat/test-makeProblem.r
@@ -1,23 +1,31 @@
 context("test makeProblem()")
 
 sp <- searchpaths()
-fn <- paste(sp[grep("sdcTable", sp)], "/data/microData1.RData", sep="")
+fn <- file.path(sp[grep("sdcTable", sp)], "data", "microData1.RData")
 microData <- get(load(fn))
-dim.region <- data.frame(levels=c('@','@@','@@','@@','@@'), codes=c('Total', 'A','B','C','D'), stringsAsFactors=FALSE)
-dim.gender <- data.frame(levels=c('@','@@','@@'), codes=c('Total', 'male','female'), stringsAsFactors=FALSE)
-dimList <- list(region=dim.region, gender=dim.gender)
-dimVarInd <- c(1,2)
+dim.region <- data.frame(
+  levels = c("@", "@@", "@@", "@@", "@@"),
+  codes = c("Total", "A", "B", "C", "D"),
+  stringsAsFactors = FALSE
+)
+dim.gender <- data.frame(
+  levels = c("@", "@@", "@@"),
+  codes = c("Total", "male", "female"),
+  stringsAsFactors = FALSE
+)
+dimList <- list(region = dim.region, gender = dim.gender)
+dimVarInd <- c(1, 2)
 freqVarInd <- numVarInd <- weightInd <- sampWeightInd <- NULL
 
 problem <- makeProblem(
-  data=microData,
-  dimList=dimList,
-  dimVarInd=dimVarInd,
-  freqVarInd=freqVarInd,
-  numVarInd=numVarInd,
-  weightInd=weightInd,
-  sampWeightInd=sampWeightInd)
+  data = microData,
+  dimList = dimList,
+  dimVarInd = dimVarInd,
+  freqVarInd = freqVarInd,
+  numVarInd = numVarInd,
+  weightInd = weightInd,
+  sampWeightInd = sampWeightInd
+)
 
-expect_that(problem, is_a("sdcProblem"))
-expect_that(get.problemInstance(problem@problemInstance, "nrVars"), equals(15))
-
+expect_is(problem, "sdcProblem")
+expect_equal(get.problemInstance(problem@problemInstance, "nrVars"), 15)
